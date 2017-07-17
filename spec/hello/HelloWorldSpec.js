8a470f640bcc92892e1b0da9e5c76f947d916719
zhangyouxin <zhangyouxingg@126.com>
2017-07-17 16:26:01 +0800

    fizzbuzzwhizz

--- spec/hello/HelloWorldSpec.js
+++ spec/hello/HelloWorldSpec.js
@@ -5,3 +5,37 @@ describe("test suite: Hello world", function () {
     });
 
 });
+describe("test suite: isMultiple3", function () {
+
+    it("test case 2: should return true", function () {
+        expect(isMultiple3(3)).toEqual(0);
+    });
+
+});
+describe("test suite: FizzBuzzWhizz", function () {
+
+    it("test case 1: 105 should return FizzBuzzWhizz", function () {
+        expect(FizzBuzzWhizz(105)).toEqual("FizzBuzzWhizz");
+    });
+    it("test case 2: 210 should return FizzBuzzWhizz", function () {
+        expect(FizzBuzzWhizz(210)).toEqual("FizzBuzzWhizz");
+    });
+    it("test case 3: 3 should return Fizz", function () {
+        expect(FizzBuzzWhizz(3)).toEqual("Fizz");
+    });
+    it("test case 4: 35 should return BuzzWhizz", function () {
+        expect(FizzBuzzWhizz(35)).toEqual("BuzzWhizz");
+    });
+    it("test case 5: 21 should return FizzWhizz", function () {
+        expect(FizzBuzzWhizz(21)).toEqual("FizzWhizz");
+    });
+    it("test case 6: 15 should return FizzBuzz", function () {
+        expect(FizzBuzzWhizz(15)).toEqual("FizzBuzz");
+    });
+    it("test case 7: 1 should return 1", function () {
+        expect(FizzBuzzWhizz(1)).toEqual(1);
+    });
+    it("test case 8: -1 should return error", function () {
+        expect(FizzBuzzWhizz(-1)).toEqual("input error");
+    });
+});
--- src/hello/HelloWorld.js
+++ src/hello/HelloWorld.js
@@ -5,7 +5,42 @@ function helloWorld() {
     // commets
 
 
+
+
+
+
     return "Hello world!"
 }
 
 
+function  isMultiple3( digit) {
+    return digit%3;
+}
+
+function  isMultiple5( digit) {
+    return digit%5;
+}
+
+function  isMultiple7( digit) {
+    return digit%7;
+}
+
+function FizzBuzzWhizz( input) {
+    if(input<=0){
+        return "input error";
+    }
+    var fbz = "";
+    if(isMultiple3(input)===0){
+        fbz += "Fizz";
+    }
+    if(isMultiple5(input)===0){
+        fbz += "Buzz";
+    }
+    if(isMultiple7(input)===0){
+        fbz += 'Whizz';
+    }
+    if(fbz == ""){
+        fbz = input;
+    }
+    return fbz;
+}
